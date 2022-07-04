46eb074c112aace07f5278ce843849e1712d3310
olamiji-8 <olamijisotomi@gmail.com>
2022-07-04 17:54:54 +0100

    New changes

---
+++ src/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from '../Components/Footer/Footer'
+import Header from '../Components/Header/Header'
+
+const Home = () => {
+  return (
+    <div>
+        <Header/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home
