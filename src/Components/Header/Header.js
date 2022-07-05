b5dc4c64369b796eec35a187af3e5d04d3eaca6f
olamiji-8 <olamijisotomi@gmail.com>
2022-07-05 13:42:17 +0100

    Header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
 
 const Header = () => {
   return (
-    <div>
+    <div className='Header'>
       <img src='./assest/images/Airbnb_logo.png' alt='' className='Header-icon'/>
+      <div className='Header-search'>
+        <input type="Search"/>
+        <SearchIcon/>
+      </div>
+      <div className='Header-right'>
+        <p>Become a Host</p>
+      </div>
     </div>
   )
 }
