34f03ebdb70b083b8cecbaa89b26e9eaad52ee63
olamiji-8 <olamijisotomi@gmail.com>
2022-07-07 11:38:53 +0100

    cards

---
+++ src/Components/Search/List.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './List.css'
+import Cards from '../Cards/Cards'
+
+const List = () => {
+  return (
+    <div>
+      <div>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+      </div>
+      <div>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+      </div>
+      <div>
+        <Cards/>
+        <Cards/>
+        <Cards/>
+      </div>
+     
+
+    </div>
+  )
+}
+
+export default List
