571a1d1fb46edc64ac7b9e64760f462823e6a794
olamiji-8 <olamijisotomi@gmail.com>
2022-07-07 19:32:01 +0100

    Updated

--- src/Components/Search/List.js
+++ src/Components/Search/List.js
@@ -1,90 +1,90 @@
-// import React from 'react'
-// import './List.css'
-// import Cards from '../Cards/Cards'
+import React from 'react'
+import './List.css'
+import Cards from '../Cards/Cards'
 
-// function List () {
-//   return (
-//     <div>
-//       <p>Hi</p>
-//       <div>
-//         <Cards
-//         src="./assest/images/island/1_island.webp"
-//         title="Badachro, UK"
-//         decription="5,754 kilometers away Nov 12 – 17"
-//         price="199 CHF"
-//          />
-//         <Cards
+function List () {
+  return (
+    <div>
+      <p>Hi</p>
+      <div>
+        <Cards
+        src="./assest/images/island/1_island.webp"
+        title="Badachro, UK"
+        decription="5,754 kilometers away Nov 12 – 17"
+        price="199 CHF"
+         />
+        <Cards
 
-//         title="Sali, Croatia, Croatia"
-//         description="4,316 kilometers away Sep 19 – 24"
-//         price="147 CHF"
+        title="Sali, Croatia, Croatia"
+        description="4,316 kilometers away Sep 19 – 24"
+        price="147 CHF"
         
         
-//         />
-//         <Cards
+        />
+        <Cards
 
-//         title= "Murter, Croatia"
-//         description="4,323 kilometers away Oct 16 – 23"
-//         price="159 CHF"
+        title= "Murter, Croatia"
+        description="4,323 kilometers away Oct 16 – 23"
+        price="159 CHF"
         
         
         
-//         />
-//       </div>
-//       <div>
-//         <Cards
+        />
+      </div>
+      <div>
+        <Cards
 
-//         title="Vis, Croatia"
-//         description="4,262 kilometers away Jul 13 – 19"
-//         price="1,520 CHF"
+        title="Vis, Croatia"
+        description="4,262 kilometers away Jul 13 – 19"
+        price="1,520 CHF"
        
-//         />
-//         <Cards
+        />
+        <Cards
 
-//         title="Angra dos Reis, Brazil"
-//         description="6,135 kilometers away Jul 22 – 27"
-//         price="681 CHF"
+        title="Angra dos Reis, Brazil"
+        description="6,135 kilometers away Jul 22 – 27"
+        price="681 CHF"
         
-//         />
-//         <Cards
+        />
+        <Cards
 
-//         title="Općina Lastovo, Croatia"
-//         description="4,236 kilometers away Sep 1 – 7"
-//         price="124 CHF"
+        title="Općina Lastovo, Croatia"
+        description="4,236 kilometers away Sep 1 – 7"
+        price="124 CHF"
         
-//         />
-//       </div>
-//       <div>
-//         <Cards
+        />
+      </div>
+      <div>
+        <Cards
 
-//         title="Uusikaupunki, Finland"
-//         description="6,227 kilometers away Aug 24 – 29"
-//         price="226 CHF"
+        title="Uusikaupunki, Finland"
+        description="6,227 kilometers away Aug 24 – 29"
+        price="226 CHF"
         
         
-//         />
-//         <Cards 
+        />
+        <Cards 
 
-//         title="Gildeskål, Norway"
-//         decription="6,797 kilometers away Jul 24 – 29"
-//         price="215 CHF"
+        title="Gildeskål, Norway"
+        decription="6,797 kilometers away Jul 24 – 29"
+        price="215 CHF"
         
-//         />
-//         <Cards
+        />
+        <Cards
 
-//         title="Skålen, Sweden"
-//         description="6,014 kilometers away Oct 1 – 8"
-//         price="211 CHF"
+        title="Skålen, Sweden"
+        description="6,014 kilometers away Oct 1 – 8"
+        price="211 CHF"
         
-//         />
-//       </div> 
+        />
+      </div> 
       
-//         <p>Hi</p>
-//     </div>
-//   )
-// }
+        <p>Hi</p>
+    </div>
+  )
+}
 
-// export default List
+export default List
 
 
 
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -11,7 +11,6 @@ const Home = () => {
   return (
     <div>
         <Header/>
-        {/* <hr/> */}
         <Categories/>
         <Cards/>
         <Footer/>
