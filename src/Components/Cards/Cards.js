e5c2885a1b814739d5445c74e0986e4c525ed52d
olamiji-8 <olamijisotomi@gmail.com>
2022-07-09 13:38:22 +0100

    changed

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -2,21 +2,47 @@ import React from 'react'
 import './Cards.css'
 
 
- function Cards   ({src,title,description,price})
- {
+//  function Cards   ({src,title,description,price})
+//  {
 
-  return (
-    <div className='Cards'>
-        <img src={src} alt=''/>
-        <div>
-            <h2>{title}</h2>
-            <h4>{description}</h4>
-            <h3>{price}</h3>
-        </div>
+//   return (
+//     <div className='Cards'>
+//         <img src={img} alt=''/>
+//         <div>
+//             <h2>{title}</h2>
+//             <h4>{description}</h4>
+//             <h3>{price}</h3>
+//         </div>
    
 
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
-export default Cards
+// export default Cards
+
+const Cards =({item}) =>{
+    return(
+        <div>
+            {
+            item.map((cardItem) =>{
+                const{ id,title,img,description,price} =cardItem;
+                return(
+                    <div>
+                       <img src={img} alt=''/>
+                            <div>
+                                <h2>{title}</h2>
+                                <h4>{description}</h4>
+                                <h3>{price}</h3>
+                            </div>
+                                
+                    </div>
+                    
+                )
+            })
+        }
+        
+            </div>
+    )
+ }
+export default Cards;
--- src/Components/Categories/Categories.js
+++ src/Components/Categories/Categories.js
@@ -2,66 +2,63 @@ import React from 'react'
 import Cards from '../Cards/Cards';
 import './Categories.css'
 
-function Categories() {
-  const filterResult =(catItem) =>{
-    const result = Cards.fillter((curDate) =>{
-      return curDate.Cards === catItem;
-    });
-       filterResult(result)
-  }
+  const Categories =({categories,filterItem})=>{
+    return( categories.map((category,index))=>{
+      
+    }
   return (
     <div className='Categories'>
-      <button onClick={() => filterResult("island_section")}>
+      <button key={index} onClick={() => filterItem("category")}>
       <img src='./assest/images/1.jpg' alt='' className=''/>
       <p>island</p>
       </button>
 
-      <button onClick={() => filterResult("parks_section")}>
+      <button key={index} onClick={() => filterItem("category")}>
       <img src='./assest/images/2.jpg' alt='' className=''/>
       <p>parks</p>
       </button>
 
-      <button onClick={() => filterResult("pools_section")}>
+      <button key={index} onClick={() => filterItem("category")}>
       <img src='./assest/images/3.jpg' alt='' className=''/>
       <p>pools</p>
       </button>
 
-      <button onClick={() => filterResult("omg_section")}>
+      <button key={index} onClick={() => filterItem("category")}>
       <img src='./assest/images/4.jpg' alt='' className=''/>
       <p>omg</p>
       </button>
 
-      <button onClick={() => filterResult("beach_section")}>
+      <button key={index} onClick={() => filterItem("category")}>
       <img src='./assest/images/5.jpg' alt='' className=''/>
       <p>beach</p>
       </button>
 
-      <button onClick={() => filterResult("home_section")}>
+      <button key={index} onClick={() => filterItem("category")}>
       <img src='./assest/images/6.jpg' alt='' className=''/>
       <p>home</p>
       </button>
 
-      <button onClick={() => filterResult("design_section")}>
+      <button key={index} onClick={() => filterItem("category")}>
       <img src='./assest/images/7.jpg' alt='' className=''/>
       <p>design</p>
       </button>
 
-      <button onClick={() => filterResult("artic_section")}>
+      <button key={index} onClick={() => filterItem("category")}>
       <img src='./assest/images/8.jpg' alt='' className=''/> 
       <p>artic</p>
       </button>
 
-      <button onClick={() => filterResult("caves_section")}>
+      <button key={index} onClick={() => filterItem("category")}>
       <img src='./assest/images/9.jpg' alt='' className=''/>
       <p>caves</p>
       </button>
 
-      <button onClick={() => filterResult("brekfast_section")}>
+      <button key={index} onClick={() => filterItem("category")}>
       <img src='./assest/images/10.jpg' alt='' className=''/>
       <p>breakfast</p>
       </button>
 
-      <button onClick={() => filterResult("surfing_section")}>
+      <button key={index} onClick={() => filterItem("category")}>
       <img src='./assest/images/11.jpg' alt='' className=''/>
       <p>surfing</p>
       </button>
