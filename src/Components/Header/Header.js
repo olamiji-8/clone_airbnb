60e7cd1990044018c7ad8fa2fad9f5a6378c1ace
olamiji-8 <olamijisotomi@gmail.com>
2022-08-26 04:12:43 +0100

    update

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,10 +4,16 @@ import SearchIcon from '@mui/icons-material/Search';
 import LanguageIcon from '@mui/icons-material/Language';
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+
 
 
 const Header = ({setkey}) => {
   return (
+    <Navbar bg="light" expand="lg">
+      <Container>
     <div className='Header'>
       <img src='./assest/images/Airbnb_logo.png' alt='' className='Header-icon'/>
       <div className='Header-search'>
@@ -24,6 +30,8 @@ const Header = ({setkey}) => {
 
       </div>
     </div>
+    </Container>
+    </Navbar>
   )
 }
 
